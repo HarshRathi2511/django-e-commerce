9b296bbf08567e95aa719e8004b34a1b9e53e449
harsh <f20200794@pilani.bits-pilani.ac.in>
2022-01-02 22:44:10 +0530

    basic styling and displaying the products grid

--- store/views.py
+++ store/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Category,Product
 
+
+def categories(request): #make this accessible in every template 
+    #register this view in settings.py
+    return {
+        'categories':Category.objects.all()
+    }
 # Create your views here.
 def home_products(request):   #HttpRequest instance 
     print(request.method)
     products = Product.objects.all()
     return render(request,'store/home.html',{'products':products})
-
     #render returns an HttpResponse while request is an HttpRequest class
