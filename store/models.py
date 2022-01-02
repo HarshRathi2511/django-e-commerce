929ae9dfb02a8e1493364d43a2c604bc7f76cb8f
harsh <f20200794@pilani.bits-pilani.ac.in>
2022-01-02 09:22:59 +0530

    models and images folder created

--- store/models.py
+++ store/models.py
@@ -1,7 +1,32 @@
 from django.db import models
 
+from vendor.models import Vendor
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length =250,db_index=True)
-    slug = models.SlugField(max_length=255,unique=True)   
-    #the url with which a category is accessed
+    slug = models.SlugField(max_length=255,unique=True) #the url with which a category is accessed
+
+    class Meta:
+        verbose_name_plural ='categories'
+
+    def __str__(self):
+        return self.name 
+
+# The related_name attribute specifies the name of the reverse relation from the Category
+#  model back to your Product,otherwise default product_set()
+class Product(models.Model):
+    category = models.ForeignKey(Category,related_name='product',on_delete=models.CASCADE)
+    created_by = models.ForeignKey(Vendor,related_name='product_creator',on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to= 'images/')
+    slug = models.SlugField(max_length=250)
+    in_stock= models.BooleanField(default=True)
+    created_date_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering=['-created_date_time']
+
+    def __str__(self):
+        return self.title  
---
+++ vendor/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.db.models.base import Model
+from django.contrib.auth.models import User
+# Create your models here.
+class Vendor(models.Model):
+      shop_name = models.CharField(max_length=250)
+      owner= models.OneToOneField(User,related_name = 'vendor',on_delete=models.CASCADE)
+
+     
