a2244bcb2f78cc807b0d8debdf55405cc8620cef
harsh <f20200794@pilani.bits-pilani.ac.in>
2022-01-04 16:57:45 +0530

    cart prices logic done

--- cart/models.py
+++ cart/models.py
@@ -9,8 +9,12 @@ class OrderItem(models.Model):  # link between the product and the order
     quantity = models.PositiveIntegerField(default=1)
     user= models.ForeignKey(User,on_delete=models.CASCADE)
     ordered= models.BooleanField(default=False)
+
     def __str__(self):
         return f"{self.quantity} :- {self.item.title}"
+
+    def get_price_of_product(self):
+        return self.quantity*self.item.price    
  
 
 # final cart
@@ -22,5 +26,17 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order by :-{self.user.email}"
+
+    def get_num_of_cart_items(self):
+        return self.items.all().count()    
+
+    def get_total_price_of_cart(self):
+        total_price_cart=0
+        for item in self.items.all():
+            total_price_cart=total_price_cart+item.get_price_of_product()
+        return total_price_cart    
+
+
+
 
 
