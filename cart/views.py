ecdf85daabfdd00516a405af8453a0e3d94d3526
harsh <f20200794@pilani.bits-pilani.ac.in>
2022-01-31 08:25:53 +0530

    changes

--- cart/views.py
+++ cart/views.py
@@ -32,7 +32,7 @@ class OrderSummaryView(LoginRequiredMixin,View):
         try:
             if Vendor.objects.filter(created_by=self.request.user).exists:
                 messages.info(self.request,'Vendors cant buy!')
-                return redirect('store:product_detail')
+                return redirect('store:home_products')
             orders = Order.objects.get(user=self.request.user,ordered=False)
             user_detail=get_object_or_404(UserDetail,user=self.request.user)
             context={'orders':orders,'user_detail':user_detail}
@@ -48,7 +48,7 @@ class OrderSummaryView(LoginRequiredMixin,View):
 def add_to_cart(request, slug):
     if Vendor.objects.filter(created_by=request.user).exists:
         messages.info(request,'Vendors cant add products in cart')
-        return redirect('store:product_detail')
+        return redirect('store:home_products')
     # get the product and create the order item
     product = get_object_or_404(Product, slug=slug)
     # do not create new order item if it already in cart
